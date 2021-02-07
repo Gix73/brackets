9f445a8e82d848024a7d2120488cbbae6ebde393
Gix73 <gix73@yandex.ru>
2021-02-07 16:59:53 +0300

    init: solution upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let arr1 = bracketsConfig.map((str) => str.join(""));
+for (let i=0; i < arr1.length; i++){
+  if(str.includes(arr1[i])){
+    str = str.replace(arr1[i], "");
+    i = -1;
+  }
+}
+  if (str){
+    return false;
+  }else{
+    return true;
+  }
 }
